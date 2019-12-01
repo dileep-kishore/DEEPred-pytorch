7d0b887d5003829a86b0492137bd731861906319
Dileep Kishore <k.dileep1994@gmail.com>
2019-12-01 17:32:41 -0500

    Add: `split_batch` function to `io.utils`
    This function splits the batch into several mini-batches

--- deepred_pytorch/io/utils.py
+++ deepred_pytorch/io/utils.py
@@ -2,10 +2,15 @@
     Module contains utility functions for processing data
 """
 
-from typing import Any, Tuple
+from typing import Any, Iterable, Tuple
+from warnings import warn
 
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import torch
+
+
+Batch = Tuple[int, torch.Tensor, torch.Tensor]
 
 
 def normalize_data(data: np.array, scaler: Any = None) -> Tuple[np.array, Any]:
@@ -35,3 +40,33 @@ def normalize_data(data: np.array, scaler: Any = None) -> Tuple[np.array, Any]:
         scaler.fit(data)
     data_normalized = scaler.tranform(data)
     return data_normalized, scaler
+
+
+def split_batch(
+    x: torch.Tensor, y: torch.Tensor, minibatch_size: int
+) -> Iterable[Batch]:
+    """
+        Split batch into mini-batches
+        x and y must have the same number of samples
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Feature tensor for the entire batch
+        y : torch.Tensor
+            Label tensor for the entire batch
+        minibatch_size : int
+            The size of the minibatch
+
+        Returns
+        -------
+        Iterable[Batch]
+    """
+    assert x.shape[0] == y.shape[0], "x and y shapes do not match"
+    n_samples = x.shape[0]
+    if n_samples % minibatch_size != 0:
+        warn("Final batch will have fewer samples")
+    for batch_ind, ind in enumerate(range(0, n_samples, minibatch_size)):
+        x_split = x[ind : ind + minibatch_size, :]
+        y_split = y[ind : ind + minibatch_size, :]
+        yield batch_ind, x_split, y_split
