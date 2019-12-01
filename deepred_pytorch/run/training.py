77ea90c7d25fd6ade165a516175b90b79700f496
Dileep Kishore <k.dileep1994@gmail.com>
2019-12-01 18:32:07 -0500

    Add: `train` function to `run.training` module
    This trains the DEEPred classifier on the training dataset
    Returns the trained pytorch model

---
+++ deepred_pytorch/run/training.py
@@ -0,0 +1,67 @@
+"""
+    Module to train the DEEPred NN classifier
+"""
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from ..models import Model
+from ..io.utils import split_batch
+
+
+def train(
+    x_train: torch.Tensor,
+    y_train: torch.Tensor,
+    epochs: int,
+    parameters: dict,
+    minibatch_size: int = 32,
+    batchnorm: bool = True,
+    p_dropout: float = 0.5,
+) -> nn.Module:
+    """
+        Train the DEEPred pytroch classifier
+
+        Parameters
+        ----------
+        x_train : torch.Tensor
+            The tensor containing the feature vectors for training
+        y_train : torch.Tensor
+            The tensor containing the label vectors for training
+        epochs : int
+            The number of epochs for which to train the model
+        parameters : dict
+            The parameters that define the nodes of the neural network
+        minibatch_size : int, optional
+            The size of the minibatches created during training
+            Default value is 32
+        batchnorm : bool, optional
+            Flag that turns on batch normalization of the hidden layers
+            Default value is true
+        p_dropout : float, optional
+            The probability of the dropout
+            Default value is 0.5
+
+        Returns
+        -------
+        List[str]
+            The list of protein ids associated with the GO term
+    """
+    # NOTE: Normalize data before passing it into this function
+    model = Model(parameters, batchnorm=batchnorm, p_dropout=p_dropout)
+    # TODO: Need to change the loss function(?)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    model.train()
+    for epoch in range(epochs):
+        loss_per_epoch = 0.0
+        for batch_ind, x_mb, y_mb in split_batch(x_train, y_train, minibatch_size):
+            optimizer.zero_grad()
+            y_pred = model(x_mb)
+            # Need to fix the dimensions?
+            loss = criterion(y_pred, y_mb)
+            loss.backward()
+            optimizer.step()
+            loss_per_epoch += loss.item()
+        print(f"Epoch {epoch}: train loss: {loss_per_epoch}")
+    return model
