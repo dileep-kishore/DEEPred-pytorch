b60da24a477fab145449a9850a90fd3b1dce529f
Dileep Kishore <k.dileep1994@gmail.com>
2019-12-01 15:37:07 -0500

    Add: `io.utils.normalize_data` method
    Normalize data using the provided scaler object

--- deepred_pytorch/io/utils.py
+++ deepred_pytorch/io/utils.py
@@ -2,5 +2,36 @@
     Module contains utility functions for processing data
 """
 
+from typing import Any, Tuple
 
-# def normalize_data(X):
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+def normalize_data(data: np.array, scaler: Any = None) -> Tuple[np.array, Any]:
+    """
+        Normalize data using the provided scaler object
+        Uses `StandardScaler` from scikit-learn if scaler not provided
+
+        Parameters
+        ----------
+        data : np.array
+            The data to be normalized
+            Each sample must be in one row
+            Features must be along the columns
+        scaler : object, optional
+            Object used to normalize the data
+            Must have the `.transform` method
+
+        Returns
+        -------
+        data_normalized
+            The normalized data
+        scaler
+            The scaler object used to normalize the data
+    """
+    if not scaler:
+        scaler = StandardScaler()
+        scaler.fit(data)
+    data_normalized = scaler.tranform(data)
+    return data_normalized, scaler
