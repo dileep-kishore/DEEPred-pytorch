2fb8dc408e515959ba3061d0e31697c286dee352
Dileep Kishore <k.dileep1994@gmail.com>
2019-12-01 23:24:28 -0500

    Fix: Training script now works

--- deepred_pytorch/scripts/train_1_model.py
+++ deepred_pytorch/scripts/train_1_model.py
@@ -26,7 +26,7 @@ def main(
         "n_y": y_train.shape[1],
     }
     x_train_tensor = torch.FloatTensor(x_train.values)
-    y_train_tensor = torch.FloatTensor(y_train.values)
+    y_train_tensor = torch.LongTensor(y_train.values)
     x_train_tensor, scaler = normalize_data(x_train_tensor)
     model = train(
         x_train_tensor,
@@ -41,7 +41,7 @@ def main(
         feature_vector_file, model_go_map_file, go_prot_map_test_dir
     )
     x_test_tensor = torch.FloatTensor(x_test.values)
-    y_test_tensor = torch.FloatTensor(y_test.values)
+    y_test_tensor = torch.LongTensor(y_test.values)
     x_test_tensor, _ = normalize_data(x_test_tensor, scaler=scaler)
     y_test_pred_tensor = model(x_test_tensor)
     acc = balanced_accuracy(y_test_tensor, y_test_pred_tensor)
@@ -49,9 +49,7 @@ def main(
 
 
 if __name__ == "__main__":
-    FEATURE_VECTOR_FILE = pathlib.Path(
-        "data/feature_vectors/CTriad_protr_training_MF.csv"
-    )
+    FEATURE_VECTOR_FILE = pathlib.Path("data/feature_vectors/CTriad_training_MF.csv")
     MODEL_GO_MAP_FILE = pathlib.Path("data/model_go_map/MFGOTerms30_1_1001_2000_1.txt")
     GO_PROT_MAP_TRAIN_DIR = pathlib.Path("data/go_prot_map/training")
     GO_PROT_MAP_TEST_DIR = pathlib.Path("data/go_prot_map/testing")
