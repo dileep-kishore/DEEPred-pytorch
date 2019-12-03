c48adfe8ce1ccdf2836a912bac2899f100623d11
Dileep Kishore <k.dileep1994@gmail.com>
2019-12-03 17:50:26 -0500

    Fix: Add plotting capability to `perf_vs_modelsize` function

--- deepred_pytorch/visualization/visualization.py
+++ deepred_pytorch/visualization/visualization.py
@@ -6,6 +6,7 @@ import re
 import pathlib
 from typing import List
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
@@ -28,15 +29,23 @@ def perf_vs_modelsize(
         pd.DataFrame
             DataFrame containing model performance
     """
-    pattern = re.compile(r"MFGOTerms30_(.)_(.*)_(.*)_(.)_output.txt")
+    pattern = re.compile(r"MFGOTerms30_(.)_(.*)_(.*)_(.*)_output")
     data_list: List[dict] = []
     for model_output_file in model_output_dir.glob("*_output.txt"):
-        match = re.match(pattern, str(model_output_file))
+        model_name = model_output_file.stem
+        match = re.match(pattern, model_name)
         if match:
             level, size_lb, size_ub, model_num = match.groups()
         else:
             raise ValueError("Model format is incorrect")
         accuracy_dict = parse_model_output(model_output_file)
         data_list.append({**accuracy_dict, "model_size": f"{size_lb}-{size_ub}"})
-    df = pd.DataFrame(data_list)
-    print(df)
+    data_df = pd.DataFrame(data_list)
+    x_order = sorted(set(data_df["model_size"]), key=lambda x: int(x.split("-")[0]))
+    sns.boxplot(x="model_size", y="roc_auc", data=data_df, order=x_order)
+    sns.swarmplot(x="model_size", y="roc_auc", data=data_df, color=".25", order=x_order)
+    plt.xlabel("Model size")
+    plt.ylabel("ROC AUC")
+    plt.xticks(rotation=30)
+    plt.tight_layout()
+    plt.savefig(fig_path)
