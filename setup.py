cb3b4b6454db204269374209c65e6d1b5e9b8832
Dileep Kishore <k.dileep1994@gmail.com>
2019-12-02 20:09:22 -0500

    Chore: Simplify the setup.py file

--- setup.py
+++ setup.py
@@ -5,52 +5,43 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = []
 
-test_requirements = ['pytest>=3', ]
+test_requirements = []
 
 setup(
     author="Dileep Kishore, Muzamil Khan",
-    author_email='dkishore@bu.edu',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    author_email="dkishore@bu.edu",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="A pytorch implementation of the DEEPred algorithm",
-    entry_points={
-        'console_scripts': [
-            'deepred_pytorch=deepred_pytorch.cli:main',
-        ],
-    },
+    # entry_points={"console_scripts": ["deepred_pytorch=deepred_pytorch.cli:main",],},
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='deepred_pytorch',
-    name='deepred_pytorch',
-    packages=find_packages(include=['deepred_pytorch', 'deepred_pytorch.*']),
+    keywords="deepred_pytorch",
+    name="deepred_pytorch",
+    packages=find_packages(include=["deepred_pytorch", "deepred_pytorch.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/dileep-kishore/deepred_pytorch',
-    version='0.1.0',
+    url="https://github.com/dileep-kishore/deepred_pytorch",
+    version="0.1.0",
     zip_safe=False,
 )
